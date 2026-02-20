d2e507faf7012c4e81e5fbcc112fb936b106b49c
AriAnna1905 <ariannadipinto99@gmail.com>
2026-02-20 19:10:01 +0100

    aggiunti file

---
+++ script.ts
@@ -0,0 +1,21 @@
+
+interface Person {
+    name : String
+    surname : String
+    years_old : Number
+    city : String
+}
+
+
+class Artist  {
+    name = "Arianna"
+    surname = "Dipinto"
+    years_old = 26
+    city = "Barletta"
+}
+
+let Arianna = new Artist()
+console.log(Arianna.name)
+    
+
+
